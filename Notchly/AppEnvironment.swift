3c7d56eb78df20f686124c257a19e07f0bca0907
Mason Blumling <hello@masonblumling.com>
2025-04-19 20:50:52 -0700

    wth

--- Notchly/AppEnvironment.swift
+++ Notchly/AppEnvironment.swift
@@ -5,3 +5,15 @@
 //  Created by Mason Blumling on 4/19/25.
 //
 
+@MainActor
+final class AppEnvironment: ObservableObject {
+    let calendarManager: CalendarManager
+    let mediaMonitor: MediaPlaybackMonitor
+    let calendarActivityMonitor: CalendarLiveActivityMonitor
+
+    init() {
+        self.calendarManager = CalendarManager.shared ?? CalendarManager()
+        self.mediaMonitor = MediaPlaybackMonitor.shared
+        self.calendarActivityMonitor = CalendarLiveActivityMonitor(calendarManager: calendarManager)
+    }
+}
